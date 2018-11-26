0975363c68c631711588b2d8118fafc88bfe7437
Vladislav Telichko <vladislav.telichko@ya.ru>
2018-11-26 16:51:38 +0300

    Result-controller. Исправление ошибки обработки исключения

--- controllers/result.controller.js
+++ controllers/result.controller.js
@@ -14,7 +14,7 @@ module.exports = {
                 results = results.slice(start, end);
             }
             res.json(results);
-        } catch {
+        } catch (err) {
             res.status(400).json(err);
         }
     },
